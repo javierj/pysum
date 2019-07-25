489cf6c612ba393d96c5ae0f24f8db4782a1d084
JavierJ <javierj@us.es>
2019-07-25 09:20:26 +0200

    Files for packaing

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysum-your-username",
+    version="0.0.1",
+    author="Javier J.",
+    author_email="noemail@noemail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/javierj/pysum",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
--- test/test_sum.py
+++ test/test_sum.py
@@ -3,3 +3,6 @@ from pysum.pysum import sum
 def test_sum_two_mumbers():
     assert sum(1, 2) == 3
 
+def test_sum_zero():
+    assert sum(0, 2) == 2
+
